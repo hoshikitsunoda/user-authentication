099dab374133a1102a2b83224bad71f4b8ccabc6
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-09-25 00:36:42 -0700

    finished login logout logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,7 @@ const App: React.FC = () => {
   const [message, setMessage] = useState('')
   const [errorMessage, setErrorMessage] = useState('')
   const [accessToken, setAccessToken] = useState('')
+  const [isShown, setIsShown] = useState(false)
 
   const getPublicMessageHandler = async () => {
     try {
@@ -28,18 +29,52 @@ const App: React.FC = () => {
       setMessage(data)
     } catch (error) {
       setErrorMessage(`${error.response.status}: ${error.response.statusText}`)
-      throw new Error(error.response.statusText)
     }
   }
 
+  const toggleLoginHandler = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault()
+    setIsShown((prevState) => !prevState)
+  }
+
   return (
-    <div className="App">
-      <Login setToken={setAccessToken} />
-      <button onClick={getPublicMessageHandler}>Public Profile</button>
-      {accessToken && (
-        <button onClick={getPrivateMessageHandler}>Private Profile</button>
-      )}
-      <div>{errorMessage ? errorMessage : message}</div>
+    <div className="App max-w-5xl mx-auto my-0 flex justify-center">
+      <div className="w-1/4">
+        {!isShown && (
+          <button
+            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded m-4 mt-0"
+            onClick={toggleLoginHandler}
+          >
+            Log In
+          </button>
+        )}
+        {isShown && (
+          <Login
+            setToken={setAccessToken}
+            token={accessToken}
+            toggleLogin={toggleLoginHandler}
+          />
+        )}
+      </div>
+      <div className="w-1/4">
+        <button
+          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded m-4 mt-0"
+          onClick={getPublicMessageHandler}
+        >
+          Public Profile
+        </button>
+        {accessToken && (
+          <button
+            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded m-4 mt-0"
+            onClick={getPrivateMessageHandler}
+          >
+            Private Profile
+          </button>
+        )}
+        <div>{errorMessage ? errorMessage : message}</div>
+      </div>
     </div>
   )
 }
--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -11,6 +11,8 @@ const URL = 'http://localhost:4000'
 
 interface IProps {
   setToken: Dispatch<SetStateAction<string>>
+  token: string
+  toggleLogin: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 }
 
 interface IState {
@@ -18,11 +20,13 @@ interface IState {
   password?: string
 }
 
-const Login: React.FC<IProps> = ({ setToken }) => {
-  const [userInfo, setUserInfo] = useState<IState>({
+const Login: React.FC<IProps> = ({ setToken, token, toggleLogin }) => {
+  const initialState = {
     username: '',
     password: '',
-  })
+  }
+  const [userInfo, setUserInfo] = useState<IState>(initialState)
+  const [errorMessage, setErrorMessage] = useState('')
   const inputEl = useRef() as MutableRefObject<HTMLInputElement>
 
   const usernameInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -30,47 +34,72 @@ const Login: React.FC<IProps> = ({ setToken }) => {
     setUserInfo({ ...userInfo, [event.target.name]: event.target.value })
   }
 
-  const getMessageHandler = (
+  const viewProfileHandler = async (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
     event.preventDefault()
-    axios.post(`${URL}/login`, userInfo).then(({ data }) => {
-      setToken(data.access_token)
+    try {
+      const userData = await axios.post(`${URL}/login`, userInfo)
+      setToken(userData.data.access_token)
+    } catch (error) {
       inputEl.current.value = ''
-    })
+      setErrorMessage(`${error.response.status}: ${error.response.statusText}`)
+    }
+  }
+
+  const logoutHandler = async (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault()
+    setToken('')
+    setUserInfo({ ...userInfo, password: '' })
+    setErrorMessage('')
   }
 
   return (
     <form>
-      <div>
-        <input
-          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal m-4"
-          type="text"
-          name="username"
-          autoComplete="username"
-          placeholder="username"
-          onChange={usernameInputHandler}
-          ref={inputEl}
-        />
-      </div>
-      <div>
-        <input
-          className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal m-4"
-          type="password"
-          name="password"
-          autoComplete="current-password"
-          placeholder="password"
-          onChange={usernameInputHandler}
-          ref={inputEl}
-        />
-      </div>
+      {!token && (
+        <>
+          <div>
+            <input
+              className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal m-4"
+              type="text"
+              name="username"
+              value={userInfo.username}
+              autoComplete="username"
+              placeholder="username"
+              onChange={usernameInputHandler}
+            />
+          </div>
+          <div>
+            <input
+              className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal m-4"
+              type="password"
+              name="password"
+              autoComplete="current-password"
+              placeholder="password"
+              onChange={usernameInputHandler}
+              ref={inputEl}
+            />
+          </div>
+          <p>{errorMessage}</p>
+        </>
+      )}
       <button
         type="submit"
-        onClick={getMessageHandler}
+        onClick={token ? logoutHandler : viewProfileHandler}
         className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded m-4 mt-0"
       >
-        Log In
+        {token ? 'Log Out' : 'Log In'}
       </button>
+      {!token ? (
+        <button
+          onClick={toggleLogin}
+          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded m-4 mt-0"
+        >
+          Cancel
+        </button>
+      ) : null}
     </form>
   )
 }
